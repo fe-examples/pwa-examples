a5a0ce319fffe94a63ff87e206c64f1ca6c6b850
GaHing <396324491@qq.com>
2020-05-21 12:27:46 +0800

    Update index.js

--- a2hs/index.js
+++ a2hs/index.js
@@ -30,6 +30,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 //   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+  deferredPrompt.prompt();
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
